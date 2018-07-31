183cb8e187dd96762d0d4197a03b37a803219d8c
alpredoRuben <alpredo.tampubolon@gmail.com>
2018-07-31 12:13:23 +0700

    Setting firebase and create form signup

--- src/components/User/signup.vue
+++ src/components/User/signup.vue
@@ -1,7 +1,49 @@
 <template>
-    <div>
-        WELCOME TO SIGNUP PAGE
-    </div>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12 sm6 offset-sm3>
+
+                <v-card>
+                    <v-card-text>
+                        <v-container>
+
+                            <form @submit="onSignUp">
+                                <!-- EMAIL FIELD -->
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field name="email" label="Email" id="email" v-model="email" type="email" required></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+
+                                <!-- PASSWORD FIELD -->
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field name="password" label="Password" id="password" v-model="password" type="password" required></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+
+                                <!-- CONFIRM PASSWORD FIELD -->
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-text-field name="confirmPassword" label="Confirm Password" id="confirmPassword" v-model="confirmPassword" type="password" :rules="[comparePassword]"></v-text-field>
+                                    </v-flex>
+                                </v-layout>
+
+                                <!-- SIGN UP BUTTON SUBMIT -->
+                                <v-layout row>
+                                    <v-flex xs12>
+                                        <v-btn type="submit">Sign up</v-btn>
+                                    </v-flex>
+                                </v-layout>
+                            </form>
+
+                        </v-container>
+                    </v-card-text>
+                </v-card>
+
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
@@ -9,6 +51,18 @@ export default {
     name: 'Signup',
     data () {
         return {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    },
+    computed: {
+        comparePassword () {
+            return (this.password !== this.confirmPassword) ? 'Password do not match' : ''
+        }
+    },
+    methods: {
+        onSignUp () {
 
         }
     }
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import 'vuetify/dist/vuetify.min.css'
 import colors from 'vuetify/es5/util/colors'
 import { store } from './store/data_store'
 import DateFilter from './filter/datefilters'
+import * as firebase from 'firebase'
 
 Vue.use(Vuetify, {
     theme: {
@@ -23,11 +24,24 @@ Vue.config.productionTip = false
 
 Vue.filter('dateTemplate', DateFilter)
 
+const confbase = {
+    apiKey: "AIzaSyALF7uMj2AJWxZxcP6Cmyz3RvT4i2v5Kfo",
+    authDomain: "fbase-fc6a3.firebaseapp.com",
+    databaseURL: "https://fbase-fc6a3.firebaseio.com",
+    projectId: "fbase-fc6a3",
+    storageBucket: "fbase-fc6a3.appspot.com",
+}
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   store,
   components: { App },
+  created () {
+      firebase.initializeApp(confbase)
+  },
   template: '<App/>'
+
 })
